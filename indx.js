49d3c03cd528c1c33945bb933a7c467c714faf46
Aishwarya Patil <patilaishwarya1610@gmail.com>
2022-02-09 12:01:43 +0530

    Update indx.js

--- indx.js
+++ indx.js
@@ -4,11 +4,6 @@ const showHideContent = function(){
         document.querySelector("#pre").style.display = "none";
         document.querySelector("#lay").style.display = "none";
         document.querySelector("#vis").style.display = "none";
-        document.querySelector("#exp").style.display = "none";
-        document.querySelector("#train").style.display = "none";
-        document.querySelector("#eff").style.display = "none";
-        document.querySelector("#mod").style.display = "none";
-        document.querySelector("#yt").style.display = "none";
 
 
     });
@@ -17,11 +12,6 @@ const showHideContent = function(){
         document.querySelector("#pre").style.display = "block";
         document.querySelector("#lay").style.display = "none";
         document.querySelector("#vis").style.display = "none";
-        document.querySelector("#exp").style.display = "none";
-        document.querySelector("#train").style.display = "none";
-        document.querySelector("#eff").style.display = "none";
-        document.querySelector("#mod").style.display = "none";
-        document.querySelector("#yt").style.display = "none";
 
     });
     document.querySelector("#lds").addEventListener('click', function(){
@@ -29,77 +19,15 @@ const showHideContent = function(){
         document.querySelector("#pre").style.display = "none";
         document.querySelector("#lay").style.display = "block";
         document.querySelector("#vis").style.display = "none";
-        document.querySelector("#exp").style.display = "none";
-        document.querySelector("#train").style.display = "none";
-        document.querySelector("#eff").style.display = "none";
-        document.querySelector("#mod").style.display = "none";
-        document.querySelector("#yt").style.display = "none";
+    
     });
     document.querySelector("#v").addEventListener('click', function(){
         document.querySelector("#deep").style.display = "none";
         document.querySelector("#pre").style.display = "none";
         document.querySelector("#lay").style.display = "none";
         document.querySelector("#vis").style.display = "block";
-        document.querySelector("#exp").style.display = "none";
-        document.querySelector("#train").style.display = "none";
-        document.querySelector("#eff").style.display = "none";
-        document.querySelector("#mod").style.display = "none";
-        document.querySelector("#yt").style.display = "none";
+        
     });
-    document.querySelector("#es").addEventListener('click', function(){
-        document.querySelector("#deep").style.display = "none";
-        document.querySelector("#pre").style.display = "none";
-        document.querySelector("#lay").style.display = "none";
-        document.querySelector("#vis").style.display = "none";
-        document.querySelector("#exp").style.display = "block";
-        document.querySelector("#train").style.display = "none";
-        document.querySelector("#eff").style.display = "none";
-        document.querySelector("#mod").style.display = "none";
-        document.querySelector("#yt").style.display = "none";
-    });
-    document.querySelector("#tlm").addEventListener('click', function(){
-        document.querySelector("#deep").style.display = "none";
-        document.querySelector("#pre").style.display = "none";
-        document.querySelector("#lay").style.display = "none";
-        document.querySelector("#vis").style.display = "none";
-        document.querySelector("#exp").style.display = "none";
-        document.querySelector("#train").style.display = "block";
-        document.querySelector("#eff").style.display = "none";
-        document.querySelector("#mod").style.display = "none";
-        document.querySelector("#yt").style.display = "none";
-    });
-    document.querySelector("#edl").addEventListener('click', function(){
-        document.querySelector("#deep").style.display = "none";
-        document.querySelector("#pre").style.display = "none";
-        document.querySelector("#lay").style.display = "none";
-        document.querySelector("#vis").style.display = "none";
-        document.querySelector("#exp").style.display = "none";
-        document.querySelector("#train").style.display = "none";
-        document.querySelector("#eff").style.display = "block";
-        document.querySelector("#mod").style.display = "none";
-        document.querySelector("#yt").style.display = "none";
-    });
-    document.querySelector("#sp").addEventListener('click', function(){
-        document.querySelector("#deep").style.display = "none";
-        document.querySelector("#pre").style.display = "none";
-        document.querySelector("#lay").style.display = "none";
-        document.querySelector("#vis").style.display = "none";
-        document.querySelector("#exp").style.display = "none";
-        document.querySelector("#train").style.display = "none";
-        document.querySelector("#eff").style.display = "none";
-        document.querySelector("#mod").style.display = "block";
-        document.querySelector("#yt").style.display = "none";
-    });
-    document.querySelector("#tv").addEventListener('click', function(){
-        document.querySelector("#deep").style.display = "none";
-        document.querySelector("#pre").style.display = "none";
-        document.querySelector("#lay").style.display = "none";
-        document.querySelector("#vis").style.display = "none";
-        document.querySelector("#exp").style.display = "none";
-        document.querySelector("#train").style.display = "none";
-        document.querySelector("#eff").style.display = "none";
-        document.querySelector("#mod").style.display = "none";
-        document.querySelector("#yt").style.display = "block";
-    });
+   
 }
 showHideContent();
