39762639f422e59250969d9b9d4048f5d18777c0
Shivani Shenai <shivani.shenai2000@gmail.com>
2022-02-09 12:47:06 +0530

    Added onLoad function

--- indx.js
+++ indx.js
@@ -1,33 +1,53 @@
-const showHideContent = function(){
-    document.querySelector("#dlm").addEventListener('click', function(){
-        document.querySelector("#deep").style.display = "block";
-        document.querySelector("#pre").style.display = "none";
-        document.querySelector("#lay").style.display = "none";
-        document.querySelector("#vis").style.display = "none";
+const showHideContent = function(x){
 
-
-    });
-    document.querySelector("#ptm").addEventListener('click', function(){
+    if(x==0)
+    {
+        document.querySelector("#dlm").addEventListener('click', function(){
+            document.querySelector("#deep").style.display = "block";  
+            // document.getElementById('deephtml').innerHTML;
+        });
+    }
+    else
+    {
         document.querySelector("#deep").style.display = "none";
-        document.querySelector("#pre").style.display = "block";
-        document.querySelector("#lay").style.display = "none";
-        document.querySelector("#vis").style.display = "none";
+    }
 
-    });
-    document.querySelector("#lds").addEventListener('click', function(){
-        document.querySelector("#deep").style.display = "none";
+    if(x==1)
+    {
+        document.querySelector("#ptm").addEventListener('click', function(){
+            document.querySelector("#pre").style.display = "block";  
+            // document.getElementById('deephtml').innerHTML;
+        });
+    }
+    else
+    {
         document.querySelector("#pre").style.display = "none";
-        document.querySelector("#lay").style.display = "block";
-        document.querySelector("#vis").style.display = "none";
+    }
     
-    });
-    document.querySelector("#v").addEventListener('click', function(){
-        document.querySelector("#deep").style.display = "none";
-        document.querySelector("#pre").style.display = "none";
+    
+    if(x==2)
+    {
+        document.querySelector("#lds").addEventListener('click', function(){
+            document.querySelector("#lay").style.display = "block";        
+        });
+    }
+    else
+    {
         document.querySelector("#lay").style.display = "none";
-        document.querySelector("#vis").style.display = "block";
-        
-    });
+    }
+
+    if(x==3)
+    {
+        document.querySelector("#v").addEventListener('click', function(){
+            document.querySelector("#vis").style.display = "block";          
+        });
+    }
+    else
+    {
+        document.querySelector("#vis").style.display = "none";
+    }
    
 }
+
+
 showHideContent();
