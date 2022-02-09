cb91b42b7bd4931afed5e68283a473a35e72082c
Aishwarya Patil <patilaishwarya1610@gmail.com>
2022-02-09 11:37:12 +0530

    indx.js created

---
+++ indx.js
@@ -0,0 +1,105 @@
+const showHideContent = function(){
+    document.querySelector("#dlm").addEventListener('click', function(){
+        document.querySelector("#deep").style.display = "block";
+        document.querySelector("#pre").style.display = "none";
+        document.querySelector("#lay").style.display = "none";
+        document.querySelector("#vis").style.display = "none";
+        document.querySelector("#exp").style.display = "none";
+        document.querySelector("#train").style.display = "none";
+        document.querySelector("#eff").style.display = "none";
+        document.querySelector("#mod").style.display = "none";
+        document.querySelector("#yt").style.display = "none";
+
+
+    });
+    document.querySelector("#ptm").addEventListener('click', function(){
+        document.querySelector("#deep").style.display = "none";
+        document.querySelector("#pre").style.display = "block";
+        document.querySelector("#lay").style.display = "none";
+        document.querySelector("#vis").style.display = "none";
+        document.querySelector("#exp").style.display = "none";
+        document.querySelector("#train").style.display = "none";
+        document.querySelector("#eff").style.display = "none";
+        document.querySelector("#mod").style.display = "none";
+        document.querySelector("#yt").style.display = "none";
+
+    });
+    document.querySelector("#lds").addEventListener('click', function(){
+        document.querySelector("#deep").style.display = "none";
+        document.querySelector("#pre").style.display = "none";
+        document.querySelector("#lay").style.display = "block";
+        document.querySelector("#vis").style.display = "none";
+        document.querySelector("#exp").style.display = "none";
+        document.querySelector("#train").style.display = "none";
+        document.querySelector("#eff").style.display = "none";
+        document.querySelector("#mod").style.display = "none";
+        document.querySelector("#yt").style.display = "none";
+    });
+    document.querySelector("#v").addEventListener('click', function(){
+        document.querySelector("#deep").style.display = "none";
+        document.querySelector("#pre").style.display = "none";
+        document.querySelector("#lay").style.display = "none";
+        document.querySelector("#vis").style.display = "block";
+        document.querySelector("#exp").style.display = "none";
+        document.querySelector("#train").style.display = "none";
+        document.querySelector("#eff").style.display = "none";
+        document.querySelector("#mod").style.display = "none";
+        document.querySelector("#yt").style.display = "none";
+    });
+    document.querySelector("#es").addEventListener('click', function(){
+        document.querySelector("#deep").style.display = "none";
+        document.querySelector("#pre").style.display = "none";
+        document.querySelector("#lay").style.display = "none";
+        document.querySelector("#vis").style.display = "none";
+        document.querySelector("#exp").style.display = "block";
+        document.querySelector("#train").style.display = "none";
+        document.querySelector("#eff").style.display = "none";
+        document.querySelector("#mod").style.display = "none";
+        document.querySelector("#yt").style.display = "none";
+    });
+    document.querySelector("#tlm").addEventListener('click', function(){
+        document.querySelector("#deep").style.display = "none";
+        document.querySelector("#pre").style.display = "none";
+        document.querySelector("#lay").style.display = "none";
+        document.querySelector("#vis").style.display = "none";
+        document.querySelector("#exp").style.display = "none";
+        document.querySelector("#train").style.display = "block";
+        document.querySelector("#eff").style.display = "none";
+        document.querySelector("#mod").style.display = "none";
+        document.querySelector("#yt").style.display = "none";
+    });
+    document.querySelector("#edl").addEventListener('click', function(){
+        document.querySelector("#deep").style.display = "none";
+        document.querySelector("#pre").style.display = "none";
+        document.querySelector("#lay").style.display = "none";
+        document.querySelector("#vis").style.display = "none";
+        document.querySelector("#exp").style.display = "none";
+        document.querySelector("#train").style.display = "none";
+        document.querySelector("#eff").style.display = "block";
+        document.querySelector("#mod").style.display = "none";
+        document.querySelector("#yt").style.display = "none";
+    });
+    document.querySelector("#sp").addEventListener('click', function(){
+        document.querySelector("#deep").style.display = "none";
+        document.querySelector("#pre").style.display = "none";
+        document.querySelector("#lay").style.display = "none";
+        document.querySelector("#vis").style.display = "none";
+        document.querySelector("#exp").style.display = "none";
+        document.querySelector("#train").style.display = "none";
+        document.querySelector("#eff").style.display = "none";
+        document.querySelector("#mod").style.display = "block";
+        document.querySelector("#yt").style.display = "none";
+    });
+    document.querySelector("#tv").addEventListener('click', function(){
+        document.querySelector("#deep").style.display = "none";
+        document.querySelector("#pre").style.display = "none";
+        document.querySelector("#lay").style.display = "none";
+        document.querySelector("#vis").style.display = "none";
+        document.querySelector("#exp").style.display = "none";
+        document.querySelector("#train").style.display = "none";
+        document.querySelector("#eff").style.display = "none";
+        document.querySelector("#mod").style.display = "none";
+        document.querySelector("#yt").style.display = "block";
+    });
+}
+showHideContent();
